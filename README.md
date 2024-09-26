14ba4572708444d2facd923e7867ac8396185438
Lucian <lgutu@amberstudio.com>
2024-09-26 10:00:12 +0300

    added two more models

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ pip install -r requirements.txt
 
 3. Configure the Google API KEY
 
-For creating a Google API KEY, take a look [here](https://ai.google.dev/gemini-api/docs/oauth)
+For creating a Google API KEY, take a look [here](https://ai.google.dev/gemini-api/docs/oauth) or [here](https://aistudio.google.com/app/plan_information)
 
 ```shell
 echo "GOOGLE_API_KEY='<API_KEY>' > .env"
@@ -29,6 +29,20 @@ echo "GOOGLE_API_KEY='<API_KEY>' > .env"
 python ./gemini_calculator.py
 ```
 
+5. CLI ARGS
+
+```shell
+python ./gemini_calculator.py --help
+usage: gemini_calculator.py [-h] [--gemini_model GEMINI_MODEL]
+
+Get model pricing for a Gemini model.
+
+options:
+  -h, --help            show this help message and exit
+  --gemini_model GEMINI_MODEL
+                        The gemini model to get pricing for (default: gemini-1.5-flash)
+                        Available choices: gemini-1.5-flash, gemini-1.5-pro or gemini-1.0-pro
+```
 
 ### Example Usage
 
@@ -45,7 +59,7 @@ Output costs: $0.00000240
 Total cost: $0.00000300
 ```
 
-2. Run interactively
+2. Run interactively (with the default flash model)
 
 ```shell
 python ./gemini_calculator.py
@@ -57,6 +71,32 @@ output_tokens=8
 Input costs: $0.00000060
 Output costs: $0.00000240
 Total cost: $0.00000300
+```
+
+3. Run with `gemini-1.5-pro` model
+
+```shell
+python ./gemini_calculator.py --gemini_model gemini-1.5-pro
+Enter your prompt: what is the capital of france?
+The capital of France is **Paris**. 🇫🇷 
+
+input_tokens=8
+output_tokens=11
+Input costs: $0.00001000
+Output costs: $0.00005500
+Total cost: $0.00006500
+```
+
+4. Run with `gemini-1.0-pro` model
+
+```shell
+Enter your prompt: what is the capital of france?
+Paris
+input_tokens=8
+output_tokens=1
+Input costs: $0.00000400
+Output costs: $0.00000150
+Total cost: $0.00000550
 ```
 
 ### Pricing model
--- gemini_calculator.py
+++ gemini_calculator.py
@@ -2,7 +2,43 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 import sys
+import argparse
 
+parser = argparse.ArgumentParser(description='Get model pricing for a Gemini model.', formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('--gemini_model', type=str, default='gemini-1.5-flash',
+                    help='''The gemini model to get pricing for (default: gemini-1.5-flash)
+Available choices: gemini-1.5-flash, gemini-1.5-pro or gemini-1.0-pro
+                    ''')
+
+args = parser.parse_args()
+gemini_model = args.gemini_model
+
+# https://ai.google.dev/pricing
+model_pricing = {
+    "gemini-1.5-flash": {
+        "INPUT_PRICING_LOW": 0.075 / 1000000,
+        "OUTPUT_PRICING_LOW": 0.30 / 1000000,
+        "INPUT_PRICING_HIGH": 0.15 / 1000000,
+        "OUTPUT_PRICING_HIGH": 0.60 / 1000000
+    },
+    "gemini-1.5-pro": {
+        "INPUT_PRICING_LOW": 1.25 / 1000000,
+        "OUTPUT_PRICING_LOW": 5 / 1000000,
+        "INPUT_PRICING_HIGH": 2.5 / 1000000,
+        "OUTPUT_PRICING_HIGH": 10 / 1000000
+    },
+    "gemini-1.0-pro": {
+        "INPUT_PRICING_LOW": 0.5 / 1000000,
+        "OUTPUT_PRICING_LOW": 1.5 / 1000000,
+        "INPUT_PRICING_HIGH": 0.5 / 1000000,
+        "OUTPUT_PRICING_HIGH": 1.5 / 1000000
+    }
+}
+
+if gemini_model not in model_pricing:
+    print("ERROR: Invalid model choice.\n")
+    parser.print_help()
+    sys.exit(1)
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
@@ -14,15 +50,9 @@ if not sys.stdin.isatty():
 else:
     prompt = input("Enter your prompt: ").strip()
 
-# https://ai.google.dev/pricing
-INPUT_PRICING_LOW = 0.075 / 1000000
-OUTPUT_PRICING_LOW = 0.30 / 1000000
-INPUT_PRICING_HIGH = 0.15 / 1000000
-OUTPUT_PRICING_HIGH = 0.60 / 1000000
-
 # print('Available base models:', [m.name for m in genai.list_models()])
 
-model = genai.GenerativeModel("models/gemini-1.5-flash")
+model = genai.GenerativeModel(f"models/{gemini_model}")
 
 response = model.generate_content(prompt)
 
@@ -35,11 +65,11 @@ print(f'{input_tokens=}')
 print(f'{output_tokens=}')
 
 if input_tokens <= 128000:
-    input_token_cost = input_tokens * INPUT_PRICING_LOW
-    output_token_cost = output_tokens * OUTPUT_PRICING_LOW
+    input_token_cost = input_tokens * model_pricing[gemini_model]["INPUT_PRICING_LOW"]
+    output_token_cost = output_tokens * model_pricing[gemini_model]["OUTPUT_PRICING_LOW"]
 else:
-    input_token_cost = input_tokens * INPUT_PRICING_HIGH
-    output_token_cost = output_tokens * OUTPUT_PRICING_HIGH
+    input_token_cost = input_tokens * model_pricing[gemini_model]["INPUT_PRICING_HIGH"]
+    output_token_cost = output_tokens * model_pricing[gemini_model]["OUTPUT_PRICING_HIGH"]
 
 print(f'Input costs: ${input_token_cost:.8f}')
 print(f'Output costs: ${output_token_cost:.8f}')
